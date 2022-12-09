a0d2eabae07121efa6caf6ea02cbcaf621dcd34f
SherlyLove <awinjasherly@gmail.com>
2022-12-09 17:19:03 +0000

    Completed all tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print all two  digit combinations without repeating
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (!(y <= x))
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+
+				if (!(x == 8 && y == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print 3 digit combinations
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (!(y <= x))
+			{
+				for (z = 0; z < 10; z++)
+				{
+					if (!(z <= y))
+					{
+						putchar(x + '0');
+						putchar(y + '0');
+						putchar(z + '0');
+
+					if (!(x == 7 && y == 8 && z == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
